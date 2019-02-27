1a46061191192e755988463bd0fe4d8b26991bc1
Vedika Bangre <b.vedika277@gmail.com>
2019-02-27 16:59:19 +0530

    http service created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
@@ -12,7 +12,8 @@ import { LoginComponent } from './login/login.component';
     LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/services/http/http-service.ts
@@ -0,0 +1,70 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class HttpServiceProvider {
+
+    private baseUrl = ''
+
+    constructor(private http: HttpClient) {
+
+    }
+
+    /**
+     * 
+     * @param subUrl {string} - api subUrl
+     * @description This method will call HttpClient's Get method to fetch data from API
+     * @returns Promise
+     */
+    httpGet(subUrl) {
+        console.log('httpGet called');
+        return new Promise((resolve, reject) => {
+            this.http.get(this.baseUrl + subUrl).subscribe((res: any) => {
+                resolve(res);
+            }, err => {
+                reject(err);
+            });
+        })
+    }
+
+    /**
+     * 
+     * @param subUrl {string}
+     * @param body {any}
+     * @description This method will call HttpClient's Post method to post data to backend
+     * @returns Promise
+     */
+    httpPost(subUrl: string, body: any): Promise<any> {
+        console.log('httpPost called');
+
+        return new Promise((resolve, reject) => {
+            this.http.post(this.baseUrl + subUrl, body).subscribe((res) => {
+                resolve(res);
+            }, err => {
+                reject(err.error);
+            });
+        })
+
+    }
+
+    /**
+     * 
+     * @param subUrl {string}
+     * @param body {any}
+     * @description This method will call HttpClient's Put method to update data to backend
+     * @returns Promise
+     */
+    httpPut(subUrl: string, body: any) {
+        console.log('httpPut called');
+
+        return new Promise((resolve, reject) => {
+            this.http.put('url', {}).subscribe((res: any) => {
+                resolve(res);
+
+            }, err => {
+                reject(err);
+            });
+        });
+    }
+
+}
