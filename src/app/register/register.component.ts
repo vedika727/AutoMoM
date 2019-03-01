daa38dab70fd67764d111e85ea2bd6f8bb58c90d
Vedika Bangre <b.vedika277@gmail.com>
2019-03-01 11:21:16 +0530

    registration validation done

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { RegistrationDetails } from '../shared/models/register-form';
 import { AuthService } from '../core/services/authentication/auth';
+import { MustMatch } from '../shared/validators/must-match.validator';
 
 @Component({
   selector: 'app-register',
@@ -20,14 +21,18 @@ export class RegisterComponent implements OnInit {
   }
   ngOnInit() {
     this.registerForm = this.fb.group({
-      name: ['', Validators.required],
-      email: ['', Validators.required],
+      name: ['', [Validators.required, Validators.maxLength(40)]],
+      email: ['', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@capco.com$")]],
       password: ['', [Validators.required, Validators.minLength(6)]],
       cnfPassword: ['', [Validators.required, Validators.minLength(6)]]
-    });
-    this.userForm=this.registerForm.controls
+    }, 
+    {
+      validator: MustMatch('password', 'cnfPassword')
   }
-
+  );
+    this.userForm=this.registerForm.controls
+    console.log(this.registerForm)
+  }
   onSubmit() {
     this.registrationData.email=this.userForm.email.value;
     this.registrationData.name=this.userForm.name.value;
