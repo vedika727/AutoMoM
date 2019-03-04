1f69fe98530f2f0cf12860c94e2319cf31dc8a15
Mohini <mohinimulik5@gmail.com>
2019-03-04 17:08:08 +0530

    dashboard card added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  formatsDateTest: string[] = [
+    'dd/MM/yyyy',
+    ];
+    dateNow : Date = new Date();
+    dateNowISO = this.dateNow.toISOString();
 
-  constructor() { }
+    public myArray : Array<Object> = [
+      {
+        subject: "Scrum Meeting", Meeting: 'Meeting 1', to:'23 jan 2019',from : '28 feb 2019'
+      },
+      {
+        subject: "project Meeting", Meeting: 'Meeting 2',to:'23 jan 2019',from : '28 feb 2019'
+      },
+      {
+        subject: "AutoMoM Meeting", Meeting: 'Meeting 3',to:'23 jan 2019',from : '28 feb 2019'
+      },
+      {
+        subject: "daily standup Meeting", Meeting: 'Meeting 4',to:'23 jan 2019',from : '28 feb 2019'
+      },
+  ];
 
-  ngOnInit() {
+  constructor() {
+    
+   }
+
+  ngOnInit() {  
   }
-
+ 
 }
