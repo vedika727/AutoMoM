0fedf695809cd8d79e5fb42fa42a1b5154b1e1ca
Vedika Bangre <b.vedika277@gmail.com>
2019-03-14 09:30:44 +0530

    changes in authentication component

--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -11,7 +11,7 @@ export class AuthenticationComponent implements OnInit {
   
   constructor(
     private route: ActivatedRoute,
-    private router: Router) { 
+    public router: Router) { 
     
   }
 
